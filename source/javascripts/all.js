0f732ba5ef5de329808c5f9dbd11b58281e268c7
David Ray <David.Ray.WC@gmail.com>
2016-05-03 15:38:50 -0700

    add preliminary medium editor

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,21 @@
+var editCardButton = $('.edit-content');
+var saveCardButton = $('.save-content');
+
 $(document).ready(function () {
-  console.log("jQuery Works ♡")
+
+  editCardButton.on('click', function(e){
+    $(this).parent().addClass('edit-card');
+    $(this).hide();
+    saveCardButton.show();
+    e.preventDefault();
+    var editor = new MediumEditor('.edit-card');
+  });
+
+  saveCardButton.on('click', function(e){
+    $(this).parent().removeClass('edit-card').attr('contenteditable', false);
+    $(this).hide();
+    editCardButton.show();
+    e.preventDefault();
+  });
+
 });
